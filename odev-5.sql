998f072bffb3e708c1a05df4b2487ca2e53bb6cc
Ecem <ecem@Ecem-MacBook-Air.local>
2021-07-07 15:26:04 +0300

    ODEV 5

---
+++ odev-5.sql
@@ -0,0 +1,16 @@
+-- ODEV 5 --
+
+SELECT title, length FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+SELECT title, length FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+LIMIT 5;
+
+SELECT last_name, store_id FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
